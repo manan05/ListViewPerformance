e0549a78f20f1eefd5553a2785149305a14cc451
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-08-28 21:00:07 +0530

    Performance Enhanced

--- app/src/main/java/com/example/listviewperformance/MainActivity.java
+++ app/src/main/java/com/example/listviewperformance/MainActivity.java
@@ -3,12 +3,65 @@ package com.example.listviewperformance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lvCourses;
+    ArrayList<Course> courses = Course.generateNRandomCourses(100);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvCourses = findViewById(R.id.lvCourses);
+        CourseAdapter courseAdapter = new CourseAdapter();
+        lvCourses.setAdapter(courseAdapter);
+    }
+
+    class CourseAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return courses.size();
+        }
+
+        @Override
+        public Course getItem(int position) {
+            return courses.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //here we do not create another itemView to improve performance of this
+            //if convertView is null then we make new views
+            // else we reuse them !
+            if(convertView == null){
+                convertView = getLayoutInflater().inflate(
+                        R.layout.list_item_course,
+                        parent,false
+                );
+            }
+            TextView tvCourseName = convertView.findViewById(R.id.tvCourseName);
+            TextView tvTeacherName = convertView.findViewById(R.id.tvTeacherName);
+            TextView tvLectures = convertView.findViewById(R.id.tvLectures);
+
+            Course course = getItem(position);
+            tvCourseName.setText(course.getName());
+            tvTeacherName.setText(course.getTeacherName());
+            tvLectures.setText(String.valueOf(course.getLeactures())); // Because Lectures is an int value
+            return convertView;
+        }
     }
 }
